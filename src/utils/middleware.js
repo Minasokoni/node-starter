de8c83c8f13053de66046609426f0b61c9cb7ed8
Minasokoni <minasokoni@gmail.com>
2019-08-12 15:58:53 -0700

    updates

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,14 +1,15 @@
 import compression from 'compression'
+import helmet from 'helmet'
 import express from 'express'
 import { requestLog } from './logger'
 import routes from '../routes'
 
 export default (app) => {
+  app.use(helmet())
   app.use(requestLog)
   app.use(express.json())
   app.use(compression())
   app.use(express.urlencoded({ extended: true }))
   app.use('/', routes)
-  app.disable('x-powered-by')
   app.set('view engine', 'ejs')
 }
